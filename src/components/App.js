7ea8bf00ca133513b8e7137a6cf045298b29b36a
Drewisatlas <drewisatlas@gmail.com>
2018-12-12 14:26:58 -0500

    Working but not passing some tests

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,33 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = event => {
+    console.log("changing filter");
+    this.setState({
+      filters: {type: event.target.value} // this comes from the drop down menu
+    })
+  }
+
+  fetchPets = () => {
+    let query = this.state.filters.type
+    if (query === 'all') {
+      fetch('/api/pets')
+        .then(res => res.json())
+        .then(data => this.setState({pets: data}));
+    } else {
+      fetch(`/api/pets?type=${query}`)
+        .then(res => res.json())
+        .then(data => this.setState({pets: data})); //HOW DO WE GET THIS DATA
+    }
+  }
+
+  onAdoptPet = (pet) => {
+    let newPetsArr = [...this.state.pets] //mnakes a copy of our existing array
+    let adoptedPet = newPetsArr.find(p => p.id === pet.id)
+    adoptedPet.isAdopted = true;
+    this.setState({pets: newPetsArr});
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +51,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} fetchPets={this.fetchPets}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser  pets={this.state.pets} onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
